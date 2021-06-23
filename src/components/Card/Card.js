4befa9f6f3de53d3bc5a578f16cfa1ed7ecec81a
Adria Gual <u1933571@campus.udg.edu>
2021-06-23 12:57:59 +0200

    update profile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Profile from "./Profile/Profile";
 function Card() {
   return (
     <div class="w-full border-4 rounded border-purple bg-white">
       <div class="grid grid-cols-5">
-        <div class="col-span-2"></div>
+        <div class="col-span-2 p-6">
+          <Profile></Profile>
+        </div>
         <div class="col-span-3">sss</div>
       </div>
     </div>
--- src/components/Card/Profile/Profile.js
+++ src/components/Card/Profile/Profile.js
@@ -1,7 +1,17 @@
 import React from "react";
+import logo from "../../../assets/images/bottomleft.png";
 
 function Profile() {
-  return <div></div>;
+  return (
+    <div className="p-6">
+      <img
+        src={logo}
+        alt="twitch-cup-logo"
+        className="h-20 rounded-full pb-6"
+      ></img>
+      <div className="profile-card-title m-24">Ander</div>
+    </div>
+  );
 }
 
 export default Profile;
