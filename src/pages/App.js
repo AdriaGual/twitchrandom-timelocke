18e9506e2fa2657d59529ba4ee6f67cb84fac750
Adria Gual <u1933571@campus.udg.edu>
2021-06-23 20:12:45 +0200

    add google analytics

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,22 +1,30 @@
 import TopBar from "../components/TopBar/TopBar";
 import Main from "../components/Main/Main";
 import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Analytics from "react-router-ga";
 
 function App() {
   return (
-    <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Twitch Cup</title>
-        <meta
-          name="description"
-          content="Todos los equipos de la Twitch Cup 2021."
-        />
-        <link rel="canonical" href="http://twitch-cup.ga" />
-      </Helmet>
-      <TopBar></TopBar>
-      <Main></Main>
-    </div>
+    <Router>
+      <Analytics id="UA-191815221-4">
+        <Switch>
+          <Route exact path="/">
+            <Helmet>
+              <meta charSet="utf-8" />
+              <title>Twitch Cup</title>
+              <meta
+                name="description"
+                content="Todos los equipos de la Twitch Cup 2021."
+              />
+              <link rel="canonical" href="http://twitch-cup.ga" />
+            </Helmet>
+            <TopBar></TopBar>
+            <Main></Main>
+          </Route>
+        </Switch>
+      </Analytics>
+    </Router>
   );
 }
 
