fab3cc083580871712ac5e14772dcf723af08bf4
Adria Gual <u1933571@campus.udg.edu>
2021-06-23 19:37:51 +0200

    update

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,19 @@
 import TopBar from "../components/TopBar/TopBar";
 import Main from "../components/Main/Main";
-import Footer from "../components/Footer/Footer";
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Twitch Cup</title>
+        <meta
+          name="description"
+          content="Todos los equipos de la Twitch Cup 2021."
+        />
+        <link rel="canonical" href="http://twitch-cup.ga" />
+      </Helmet>
       <TopBar></TopBar>
       <Main></Main>
     </div>
