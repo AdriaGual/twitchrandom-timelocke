2094ef3e4d92156d15670f3968ca4f9e8e8537c6
Adria Gual <u1933571@campus.udg.edu>
2021-07-01 15:57:54 +0200

    update size

--- src/components/Card/Profile/Profile.js
+++ src/components/Card/Profile/Profile.js
@@ -11,13 +11,13 @@ function Profile(props) {
         ></img>
         <div className="relative pt-2">
           <div className="">
-            <div className="profile-card-title w-36 h-10 flex items-center p-2 shadow">
+            <div className="profile-card-title w-48 h-10 flex items-center p-2 shadow">
               <p className="font-semibold">{props.name}</p>
             </div>
           </div>
 
-          <div className="absolute top-9 left-24">
-            <div className="profile-card-position w-32 h-6 flex items-center p-2 text-white">
+          <div className="absolute top-10 left-24">
+            <div className="profile-card-position w-36 h-8 flex items-center p-2 text-white">
               <p className="font-semibold">
                 {props.isPlayer ? "JUGADOR/A" : "COACH"}
               </p>
