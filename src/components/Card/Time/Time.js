3502c53f85c294be21a54d76f3ee97cb1452283b
AdriaGual <u1933571@campus.udg.edu>
2021-07-18 18:58:39 +0200

    fix warnings

--- src/components/Card/Time/Time.js
+++ src/components/Card/Time/Time.js
@@ -1,9 +1,6 @@
 import React from "react";
-import Moment from "react-moment";
 
 function Time(props) {
-  const date = "2021-07-19T18:00-0500";
-
   return (
     <div>
       <p className="font-semibold text-lg">TIEMPO RESTANTE</p>
